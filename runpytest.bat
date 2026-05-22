afb745674c73a4bb280ee32375dfbf7b18334394
Andrew <andrew@johnson5.net>
2026-05-22 09:54:54 -0400

    Fix uv workflow and align host docs

--- runpytest.bat
+++ runpytest.bat
@@ -3,3 +3,5 @@ set hereOrig=%~dp0
 set here=%hereOrig%
 if #%hereOrig:~-1%# == #\# set here=%hereOrig:~0,-1%
 call uv run --group unit-tests --directory "%here%" pytest %*
+if ERRORLEVEL 5 exit /b 0
+if ERRORLEVEL 1 exit /b %ERRORLEVEL%
